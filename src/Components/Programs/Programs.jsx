352a9192c3333cf5399337c6e00c7abb61ba5e39
Adil Shabab <adilshaba@gmail.com>
2022-07-04 07:57:12 -0700

    program section completed

--- src/Components/Programs/Programs.jsx
+++ src/Components/Programs/Programs.jsx
@@ -1,16 +1,46 @@
-import React from 'react'
-import './Programs.css'
+import React from "react";
+import "./Programs.css";
 
 const Programs = () => {
   return (
     <div>
-        <div className="heading">
-            <p>EXPLORE OUR </p>
-            <p> PROGRAMS</p>
-            <p>TO SHAPE YOU</p>
+      <div className="heading">
+        <p className="m-0 stroke">EXPLORE OUR </p>
+        <p className="m-0 stroke"> PROGRAMS</p>
+        <p className="m-0 stroke">TO SHAPE YOU</p>
+      </div>
+      <div className="row p-3">
+        <div className="col-lg-3 col-xlg-3 col-md-6 col-sm-12 col-12">
+          <div className="box">
+          <i class="fa-solid fa-fire-flame-curved"></i>
+          <p>This program is suitable for you who wants to get rid of your fat and lose their weight.</p>
+          <button className="but">Join</button>
+          </div>
         </div>
+        <div className="col-lg-3 col-xlg-3 col-md-6 col-sm-12 col-12">
+          <div className="box">
+          <i class="fa-solid fa-person-running"></i>
+          <p>In this program, you are trained to do sequential moves in range of 20 until 30 minutes.</p>
+          <button className="but">Join</button>
+          </div>
+        </div>
+        <div className="col-lg-3 col-xlg-3 col-md-6 col-sm-12 col-12">
+          <div className="box">
+          <i class="fa-solid fa-dumbbell"></i>
+          <p>In this program, you are trained to improve your strength through many exercises.</p>
+          <button className="but">Join</button>
+          </div>
+        </div>
+        <div className="col-lg-3 col-xlg-3 col-md-6 col-sm-12 col-12">
+          <div className="box">
+          <i class="fa-solid fa-heart-pulse"></i>
+          <p>This program is suitable for you who wants to get rid of your fat and lose their weight.</p>
+          <button className="but">Join</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Programs
+export default Programs;
