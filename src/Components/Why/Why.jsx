7983e3ed14438e201fbfd1b3b3acc7fb483c4296
Adil Shabab <adilshaba@gmail.com>
2022-07-04 08:58:48 -0700

    why right section progressing

--- src/Components/Why/Why.jsx
+++ src/Components/Why/Why.jsx
@@ -6,12 +6,13 @@ import image3 from '../../assets/image3.png'
 import image4 from '../../assets/image4.png'
 import tick from '../../assets/tick.png'
 import adidas from '../../assets/adidas.png'
-import 
+import nike from '../../assets/nike.png'
+import nb from '../../assets/nb.png'
 
 const Why = () => {
   return (
     <div className='row py-5 mt-5 px-3'>
-        <div className="col-lg-6 col-xlg-6 col-md-12 col-sm-12 col-12">
+        <div className="d-flex justify-content-center align-items-center col-lg-6 col-xlg-6 col-md-12 col-sm-12 col-12">
             <div className="images">
                 <div className="section-1 px-2">
                     <img className='first-img' src={image1} alt="" />
@@ -31,7 +32,7 @@ const Why = () => {
                 </div>
             </div>
         </div>
-        <div className="col-lg-6 col-xlg-6 col-md-12 col-sm-12 col-12">
+        <div className="right-w d-flex flex-column col-lg-6 col-xlg-6 col-md-12 col-sm-12 col-12">
             <span>SOME REASONS</span>
             <p className="header"><span className="stroke">WHY</span> CHOOSE US ?</p>
             <div className="line">
@@ -52,9 +53,9 @@ const Why = () => {
             </div>
             <p>OUR PARTNERS</p>
             <div className="brands">
-                <img src={} alt="" />
-                <img src={} alt="" />
-                <img src={} alt="" />
+                <img src={adidas} alt="" />
+                <img src={nike} alt="" />
+                <img src={nb} alt="" />
             </div>
         </div>
     </div>
