78b705e9d90aaced9651e2f6fff7ad0e206b22db
Adil Shabab <adilshaba@gmail.com>
2022-07-05 07:57:01 -0700

    email js added

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
 import './Contact.css'
 
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 const Contact = () => {
+
+
+  const form = useRef();
+
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_w615m58', 'template_8r4wfva', form.current, '_ZF_lEXLnGlyO6WlC')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+
+
   return (
     <div className='row contact mb-5'>
         <div className="py-4 col-lg-6 col-xlg-6 col-md-12 col-sm-12 col-12 text-center">
             <p className="head stroke m-0">READY TO LEVEL UP</p>
             <p className="head stroke m-0">YOUR BODY WITH US?</p>
         </div>
-        <div className="email px-5 col-lg-6 col-xlg-6 col-md-12 col-sm-12 col-12 d-flex justify-content-center align-items-center">
+        <form ref={form} onSubmit={sendEmail} className="email px-5 col-lg-6 col-xlg-6 col-md-12 col-sm-12 col-12 d-flex justify-content-center align-items-center">
             <input className='input w-100' type="email" name="email" id="email" />
-            <button className='but'>Join</button>
-        </div>
+            <button className='but' type='submit'>Join</button>
+        </form>
     </div>
   )
 }
