99f6174b578ce3e00ab99f529d9f5d4272a2045a
Adil Shabab <adilshaba@gmail.com>
2022-07-05 08:17:41 -0700

    link added to footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,9 +9,9 @@ const Footer = () => {
     <div className='footer px-5'>
         <hr />
         <div className="flex d-flex justify-content-center align-items-center">
-            <img src={Github} alt="" />
-            <img src={Linkedin} alt="" />
-            <img src={Instagram} alt="" />
+            <a target='__blank' href="https://github.com/adil-shabab"><img src={Github} alt="" /></a>
+            <a target='__blank' href="https://www.linkedin.com/in/adil-shabab-1948b2233/"><img src={Linkedin} alt="" /></a>
+            <a target='__blank' href="https://www.instagram.com/adil___shabab_/"><img src={Instagram} alt="" /></a>
         </div>
     </div>
   )
